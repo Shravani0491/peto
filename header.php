7f7281e9c752b7add43934c2870658c215d2f770
Shravani0491 <shravani.cs21@bmsce.ac.in>
2023-10-10 06:59:55 +0530

    first commit

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ if(isset($message)){
 
    <div class="flex">
 
-      <a href="admin_page.php" class="logo">Petto</span></a>
+      <a href="admin_page.php" class="logo">Peto</span></a>
 
       <nav class="navbar">
          <a href="home.php">home</a>
